e7373577bf6019e6c1bafa7a79607a510474c43f
Jake Schwartz <jakehschwartz@gmail.com>
2020-01-06 19:51:59 -0500

    Move html for bookmark button to index.html

--- src/main/scala/reclipe/Main.scala
+++ src/main/scala/reclipe/Main.scala
@@ -1,20 +1,5 @@
 package reclipe
 
 object Main extends App {
-  println(
-    """
-      |<p>Drag this to your bookmark bar:</p>
-      |<a href="javascript: (function () {
-      |    let code = document.createElement('script');
-      |    code.setAttribute('src', 'https://jakehschwartz.github.io/reclipe/scripts/scraper.js');
-      |    code.setAttribute('type', 'text/javascript');
-      |    document.body.appendChild(code);
-      |
-      |    let execute = document.createElement('script');
-      |    execute.setAttribute('src', 'https://jakehschwartz.github.io/reclipe/scripts/launcher.js');
-      |    code.setAttribute('type', 'text/javascript');
-      |    document.body.appendChild(execute);
-      | }());">Scraper</a>
-      |""".stripMargin.trim
-  )
+  println("It's working")
 }
